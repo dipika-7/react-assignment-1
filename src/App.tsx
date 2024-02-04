22e84f730188ea6dcc8ad116c7985e47ad9c63f6
dipika-7 <dipikasuwal77@gmail.com>
2024-02-05 02:27:18 +0545

    Remove unnecessary comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './styles/Form.css';
 
 import UserActivity from './components/UserActivityForm';
-import UserForm from './components/UserDataDisplay';
-import Weather from './components/Weather';
 
 function App() {
   return (
     <div className="App">
-      {/* <Weather /> */}
       <UserActivity />
-      {/* <UserForm /> */}
     </div>
   );
 }
